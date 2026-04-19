3fae0ed6154a6362733a14a9a9ae1c09bc1353a0
eunjeong Jang <eunjeongJang@lotte.net>
2026-04-19 21:35:06 +0900

    fix: vite.config.js 설명 추가

--- week1-react/vite.config.js
+++ week1-react/vite.config.js
@@ -7,10 +7,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       // BE 서버로 프록시 - CORS 문제 해결
+      //  요청하는 경로가 /api 이면, target 경로로 전달하겠다
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),  //  만약 요청 경로의 변경이 필요하면 rewrite 옵션을 통해 경로 변경 가능
       },
     },
   },
